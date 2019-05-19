f2d6d12416a6c82dd2af69e183cd2ea0608e6678
sina <patronplatform@gmail.com>
2019-05-19 16:38:56 -0700

    Add Value to tree view

--- src/Controls/Index.java
+++ src/Controls/Index.java
@@ -1,15 +1,54 @@
 package Controls;
 
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.StackPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Index implements Initializable {
 
+    @FXML
+    private TreeView  treeView;
+    public void AddToTree(String name){
+
+            TreeItem a = new TreeItem();
+            TreeItem all = new TreeItem("All Download");
+    //        System.out.println("1");
+            //root.setExpanded(true);
+            //create child
+            TreeItem itemChild = new TreeItem("Documents");
+            itemChild.setExpanded(false);
+            //root is the parent of itemChild
+            all.getChildren().add(itemChild);
+            itemChild = new TreeItem<>("Multimedia");
+            all.getChildren().add(itemChild);
+            itemChild = new TreeItem<>("Application");
+            all.getChildren().add(itemChild);
+            itemChild = new TreeItem<>("Compress");
+            all.getChildren().add(itemChild);
+            a.getChildren().add(all);
+            TreeItem complete = new TreeItem("Complete");
+            a.getChildren().add(complete);
+            TreeItem downloading = new TreeItem("Downloading");
+            a.getChildren().add(downloading);
+            TreeItem Error = new TreeItem("Eror");
+            a.getChildren().add(Error);
+            TreeItem Q = new TreeItem("Q");
+            a.getChildren().add(Q);
+            treeView.setRoot(a);
+            treeView.setShowRoot(false);
+        }
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        AddToTree("a");
+
 
     }
 }
--- src/Models/Main.java
+++ src/Models/Main.java
@@ -5,20 +5,30 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/View/Index.fxml"));
+
+
         primaryStage.setTitle("Download Manager");
-        primaryStage.setScene(new Scene(root, 600  , 400));
+        primaryStage.setScene(new Scene(root, 835  , 500));
         primaryStage.show();
+
     }
 
 
     public static void main(String[] args) {
+
         launch(args);
     }
+
 }
